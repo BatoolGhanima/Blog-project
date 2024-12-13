c14b0c8c91469a3a0c2964a35a35aaf56e067ae5
BatoolGhanima <batool00gh@gmail.com>
2024-12-13 21:01:33 +0300

    Add project files

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+     content: [
+    "./src/**/*.{html,js}", // يبحث في جميع ملفات HTML و JavaScript داخل مجلد src
+    "./index.html",          // يشمل ملف index.html إن وجد
+  ],
+
+  theme: {
+    extend: {
+      backgroundImage : {
+        'custom-pattern': "url('/images/background.svg')",
+        'another-pattern': "url('/path-to-another-image.jpg')",
+      },
+      colors: {
+        customGreen: {
+          light : '#9EDF9C',
+          default: '#16423C',
+          dark: '#0B1C1E',
+          fulldark:'#0B1C1E'
+
+        },
+        customGray: {
+          light : '#6A9C89',
+          default: '#4D4D4D',
+        
+
+        },
+        
+
+        fontFamily: {
+          inter: ['Inter', 'sans-serif'], 
+        },
+        
+      },
+    },
+  },
+  plugins: [],
+}
